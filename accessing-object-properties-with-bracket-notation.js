189adbee66665cb500ebb4acf8f38e806c68fc08
amit4049 <amitroysunjoy@gmail.com>
2021-03-13 23:33:03 +0600

    4th commit

---
+++ accessing-object-properties-with-bracket-notation.js
@@ -0,0 +1,12 @@
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];
+
+var drinkValue = testObj["the drink"];
