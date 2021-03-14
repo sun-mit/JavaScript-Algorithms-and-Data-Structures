7f4d6603d17fd837f68bba68bc76327aa6914135
amit4049 <amitroysunjoy@gmail.com>
2021-03-14 12:12:29 +0600

    5th commit

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,28 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+
+{
+    "artist": "Amit Roy",
+    "title": "Guiter Man",
+    "release_year": 1998,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ]
+    
+  }
+
+
+  // Add a record here
+];
